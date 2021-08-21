0f38f026c6637ee7ad6d4ee354c2a445814d2d03
Amena Vadei <12438524+amenavadei@users.noreply.github.com>
2021-08-21 20:03:27 +1200

    update to 2 decimal place

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const expensesList = document.querySelector('#expenses-list');
 
 const totalExpensesOutput = document.querySelector('#total-expenses');
 
+const alertCtrl = document.querySelector('ion-alert-controller');
+
 let totalExpenses = 0;
 
 //clear button
@@ -17,9 +19,17 @@ const btnClear = () =>{
 
 btnConfirm.addEventListener('click' ,() => {
   const enterReason = reasonInput.value;
-  const enterInput = amountInput.value;
+  const enterInput = parseFloat(amountInput.value).toFixed(2);
 
    if(enterReason.trim().length <=0 || enterInput <= 0 || enterInput.trim().length <=0){
+
+       /*await alertCtrl.create({
+          message: 'Please enter valid reason and amount!',
+          header: 'Invalid Inputs',
+          buttons: ['Okay']
+         });
+
+        await alertCtrl.present();*/
        return; // if values are Invalid - stop execution 
    }
     //If values are Valid 
